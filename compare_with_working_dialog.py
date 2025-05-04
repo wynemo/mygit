58ff05c49d54f849f3ab76a389db16069d9b7441
wynemo <ggdabin@gmail.com>
2025-05-04 12:08:06 +0800

    Add comment re: future dialog removal

--- compare_with_working_dialog.py
+++ compare_with_working_dialog.py
@@ -1,14 +1,16 @@
 from PyQt6.QtWidgets import QDialog, QVBoxLayout
 from text_diff_viewer import DiffViewer
 
+#todo 去掉这个，放到提交历史旁边的tab
+
 class CompareWithWorkingDialog(QDialog):
     def __init__(self, title, old_content, new_content, parent=None):
         super().__init__(parent)
         self.setWindowTitle(title)
         self.resize(800, 600)
-        
+
         layout = QVBoxLayout(self)
         self.diff_viewer = DiffViewer()
         layout.addWidget(self.diff_viewer)
-        
-        self.diff_viewer.set_texts(old_content, new_content) 
+
+        self.diff_viewer.set_texts(old_content, new_content)
