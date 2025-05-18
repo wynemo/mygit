3643ae3a7431933c1d0b51262e08a5bad04f5eac
wynemo <ggdabin@gmail.com>
2025-05-18 22:14:45 +0800

    在主函数中添加注释，说明尝试解决窗口失焦问题的情况，确保代码可读性和维护性。

--- main.py
+++ main.py
@@ -14,8 +14,11 @@ def main():
 
     window = GitManagerWindow()
     window.show()
+
+    # 尝试解决失焦问题,启动后,窗口灰色,使用“打开文件夹”也是灰色;切换到别的程序再切回来会变好
     window.activateWindow()
     window.raise_()
+
     sys.exit(app.exec())
 
 
