86afb66fb524d095bbc4ac016fb4a76818193c1e
wynemo <ggdabin@gmail.com>
2025-06-07 22:53:17 +0800

    feat(side_bar_widget): 添加变更按钮

--- side_bar_widget.py
+++ side_bar_widget.py
@@ -6,6 +6,7 @@ from PyQt6.QtWidgets import QToolButton, QVBoxLayout, QWidget
 class SideBarWidget(QWidget):
     project_button_clicked = pyqtSignal()
     commit_button_clicked = pyqtSignal()
+    collapse_button_clicked = pyqtSignal()
 
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -69,6 +70,32 @@ class SideBarWidget(QWidget):
         self.commit_btn.clicked.connect(self._on_commit_clicked)
         layout.addWidget(self.commit_btn)
 
+        # 变更按钮
+        self.changes_btn = QToolButton()
+        self.changes_btn.setText("变更")
+        self.changes_btn.setIcon(QIcon("icons/changes.svg"))
+        self.changes_btn.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextUnderIcon)
+        self.changes_btn.setCheckable(True)
+        self.changes_btn.setChecked(False)
+        self.changes_btn.setStyleSheet("""
+            QToolButton {
+                border: none;
+                background-color: transparent;
+            }
+            QToolButton:hover {
+                background-color: #e0e0e0;
+            }
+            QToolButton:checked {
+                background-color: #2196F3;
+            }
+            QToolButton:checked:!active {
+                background-color: #cccccc;
+            }
+        """)
+        self.changes_btn.clicked.connect(self.collapse_button_clicked.emit)
+        self.changes_btn.clicked.connect(self._on_collapse_clicked)
+        layout.addWidget(self.changes_btn)
+
         layout.addStretch()
 
     def focusOutEvent(self, event):
@@ -86,3 +113,7 @@ class SideBarWidget(QWidget):
         """处理提交按钮点击事件"""
         self.commit_btn.setChecked(True)
         self.project_btn.setChecked(False)
+
+    def _on_collapse_clicked(self):
+        """处理折叠按钮点击事件"""
+        self.changes_btn.setChecked(True)
