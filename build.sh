aab5af70abcb5beddc70535010ab31801fb29443
wynemo <ggdabin@gmail.com>
2025-06-08 16:18:34 +0800

    feat(build): 添加构建脚本和 setup.py 文件以支持 py2app 打包

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 用python -m venv venv 创建虚拟环境
+# source venv/bin/activate
+# 然后pip install -r requirements.txt
+# bash build.sh 打包
+# 不用用uv弄的环境，会报错
+find . -name "._.DS_Store" -type f -delete
+find . -name ".DS_Store" -type f -delete
+git rm --cached $(git ls-files --deleted) # git 删除（下一步commit）
+rm -rf build/ dist/
+#python setup.py py2app -A
+python setup.py py2app -p pygments
