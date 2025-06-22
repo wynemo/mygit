8db3841991487a3407d285947a348c333e54f6b8
wynemo <ggdabin@gmail.com>
2025-06-22 14:15:54 +0800

    feat: 添加文件树和顶部工具栏的中文翻译

--- translations/app_zh_CN.ts
+++ translations/app_zh_CN.ts
@@ -2,16 +2,142 @@
 <!DOCTYPE TS>
 <TS version="2.1">
   <context>
+    <name>FileTreeWidget</name>
+    <message>
+      <location filename="../workspace_explorer.py" line="693" />
+      <source>文件历史</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="697" />
+      <source>切换 Git Blame 注释</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="701" />
+      <source>查看文件夹历史</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="711" />
+      <source>复制相对路径</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="715" />
+      <source>拷贝完整路径</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="719" />
+      <source>在文件管理器中打开</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="739" />
+      <source>还原</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="834" />
+      <source>历史</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
     <name>GitManagerWindow</name>
     <message>
-      <location filename="../git_manager_window.py" line="37" />
+      <location filename="../git_manager_window.py" line="428" />
+      <location filename="../git_manager_window.py" line="63" />
       <source>Git Manager</source>
       <translation>Git 管理器</translation>
     </message>
     <message>
-      <location filename="../git_manager_window.py" line="142" />
+      <location filename="../git_manager_window.py" line="208" />
       <source>commit history</source>
       <translation>提交历史</translation>
+    </message>
+  </context>
+  <context>
+    <name>TopBarWidget</name>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="43" />
+      <source>打开文件夹</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="48" />
+      <source>最近</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="54" />
+      <source>分支:</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="63" />
+      <source>新建分支</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="83" />
+      <source>设置</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="182" />
+      <location filename="../views/top_bar_widget.py" line="99" />
+      <source>隐藏左侧面板</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="119" />
+      <source>清除最近</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="171" />
+      <source>隐藏底部面板</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="175" />
+      <source>显示底部面板</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../views/top_bar_widget.py" line="186" />
+      <source>显示左侧面板</source>
+      <translation type="unfinished" />
+    </message>
+  </context>
+  <context>
+    <name>WorkspaceExplorer</name>
+    <message>
+      <location filename="../workspace_explorer.py" line="65" />
+      <source>刷新</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="109" />
+      <source>工作区文件</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="480" />
+      <source>关闭其他标签页</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="484" />
+      <source>关闭所有标签页</source>
+      <translation type="unfinished" />
+    </message>
+    <message>
+      <location filename="../workspace_explorer.py" line="563" />
+      <source>历史</source>
+      <translation type="unfinished" />
     </message>
   </context>
 </TS>
