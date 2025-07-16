e5664224ba227c2ddf087ea51e483c4db7ecfcbc
lozhang <lozhang@noreply.gitcode.com>
2025-07-16 09:44:21 +0800

    docs: 修正 build.sh 中的注释格式和错别字

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # source venv/bin/activate
 # 然后pip install -r requirements.txt
 # bash build.sh 打包
-# 不用用uv弄的环境，会报错
+# 不用 uv 弄的环境，会报错
 find . -name "._.DS_Store" -type f -delete
 find . -name ".DS_Store" -type f -delete
 git rm --cached $(git ls-files --deleted) # git 删除（下一步commit）
